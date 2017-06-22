46c5cc1b3290ea805dd74328d5ea380cfd6596da
Victor Chan <victorchan88888888@gmail.com>
2017-06-22 00:17:40 -0700

    Added listener to check url and toggle page action if page is a YouTube video

--- YouTube Controls/background.js
+++ YouTube Controls/background.js
@@ -65,3 +65,15 @@ chrome.management.onEnabled.addListener(function(info) {
         });
     }
 });
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function(    ) {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {urlPrefix: "https://www.youtube.com/watch?v"},
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
