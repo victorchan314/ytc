946c64f802922160cf4bdd42b922aa13ed255213
Victor Chan <victorchan88888888@gmail.com>
2019-09-09 11:24:42 -0700

    Do not check src url for skipping ads

--- YouTube Controls/skipAd.js
+++ YouTube Controls/skipAd.js
@@ -1,4 +1,4 @@
 var video = document.getElementsByTagName("video")[0]
-if (!video.src.startsWith("blob:https://www.youtube.com/")) {
+//if (!video.src.startsWith("blob:https://www.youtube.com/")) {
     video.currentTime = video.duration;
-}
+//}
