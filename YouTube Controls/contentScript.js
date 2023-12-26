c63a6b764d6dc1bfbfd33df7bc580c02196176a5
Victor Chan <victorchan314@users.noreply.github.com>
2023-12-26 10:50:18 -0800

    Autoskip ads with a few bugs

--- YouTube Controls/contentScript.js
+++ YouTube Controls/contentScript.js
@@ -2,10 +2,20 @@ function retrieveValue(name) {
     return document.getElementsByTagName("video")[0][name];
 }
 
-function skipAd() {
+async function skipAd() {
     if (document.querySelector("div.html5-video-player").classList.contains("ad-showing")) {
-        var video = document.getElementsByTagName("video")[0];
-        video.currentTime = video.duration;
+        console.log(`Ad source: ${document.getElementsByTagName("video")[0].src}`);
+        let skipAdButton;
+        if (skipAdButton = document.querySelector("div.ytp-ad-skip-button-slot > span.ytp-ad-skip-button-container")) {
+            console.log("Clicking skip ad button");
+            skipAdButton.click();
+            console.log("Clicked skip ad button");
+        } else {
+            console.log("Skipping video ad");
+            var video = document.getElementsByTagName("video")[0];
+            video.currentTime = video.duration;
+            console.log("Skipped video ad");
+        }
     }
 }
 
@@ -23,10 +33,26 @@ function togglePlay() {
     }
 }
 
-//var video = document.getElementsByTagName("video")[0];
-//var currentUrl = video.src;
-//video.onloadedmetadata = () => {
-//    if (this.src !== currentUrl) {
-//        currentUrl = this.src;
-//    }
-//}
+
+async function videoEventListener(event) {
+    if (event.target.src !== currentUrl) {
+        console.log(`Current: ${currentUrl}`);
+        console.log(`New: ${event.target.src}`);
+        console.log("Skipping an ad...");
+        await new Promise((r) => setTimeout(r, 100));
+        currentUrl = event.target.src;
+        await skipAd();
+        console.log("Skipped an ad...");
+    }
+    listenForVideoSrcChanges(event.target);
+}
+
+function listenForVideoSrcChanges(video) {
+    currentVideo.removeEventListener("loadedmetadata", videoEventListener);
+    currentVideo = video;
+    video.addEventListener("loadedmetadata", videoEventListener);
+}
+
+var currentVideo = document.getElementsByTagName("video")[0];
+var currentUrl = currentVideo.src;
+listenForVideoSrcChanges(currentVideo);
